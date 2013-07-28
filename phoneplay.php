7e39059f089ddd80bfe2e7e0ef949e02826a92f2
nullren <chopflop@gmail.com>
2013-07-27 18:54:43 -0700

    this is mostly useable

--- phoneplay.php
+++ phoneplay.php
@@ -1,4 +1,16 @@
-<html>
+<?php
+
+/**
+ * use sessions to remember the last path so we can keep browsing
+ * smoothly between things.
+ */
+
+session_start();
+
+$file_loaded = $_SESSION['file_loaded'];
+
+
+?><html>
 <head>
 <title>play something</title>
 <meta name = "viewport"  content = "initial-scale=1.0, user-scalable=no, width=device-width">
@@ -9,15 +21,24 @@
 <body>
 <div data-role="page">
 <div data-role="header">
-<a data-role="button" href="/">home</a>
-<a data-role="button" href="?cmd=stop">stop</a>
-<h3>play something</h3>
+<div data-role="controlgroup" data-type="horizontal">
+<a data-role="button" data-icon="home" href="?path=/home/torrents/done">home</a>
+<a data-role="button" data-inline="true" href="?cmd=pause">Play/Pause</a>
+<a data-role="button" data-inline="true" href="?cmd=stop">Stop</a>
+</div>
 </div>
 <div data-role="content">
 <?php
 
-$path = $_GET['path'];
-$cmd = $_GET['cmd'];
+/**
+ * set up the fifos
+ *
+ * ~$ cd /home/everyone/
+ * everyone$ mkdir movies
+ * everyone$ cd movies/
+ * movies$ mkfifo mplayer
+ * movies$ mkfifo xset
+ */
 
 function send_fifo_cmd($fifo, $str)
 {
@@ -34,16 +55,6 @@ function send_fifo_cmd($fifo, $str)
   return $o;
 }
 
-/**
- * set up the fifos
- *
- * ~$ cd /home/everyone/
- * everyone$ mkdir movies
- * everyone$ cd movies/
- * movies$ mkfifo mplayer
- * movies$ mkfifo xset
- */
-
 function send_mplayer_cmd($str)
 {
   return send_fifo_cmd('/home/everyone/movies/mplayer', $str);
@@ -54,38 +65,38 @@ function send_xset_cmd($str)
   return send_fifo_cmd('/home/everyone/movies/xset', $str);
 }
 
+/**
+ * these are the possible query string things. read them and do
+ * something intelligent, hopefully.
+ */
+
+$path = $_GET['path'];
+if (empty($path))
+{
+  $path = $_SESSION['path'];
+}
+
+$cmd = $_GET['cmd'];
+
 if ($cmd == 'stop')
 {
   send_mplayer_cmd("stop");
-  goto _LISTFILES;
 }
 
 if ($cmd == 'pause')
 {
   send_mplayer_cmd("pause");
-  print("<a class=\"play\" href=\"?cmd=play\">play</a>\n");
-  goto _END;
 }
 
-if ($cmd == 'play')
-{
-  send_mplayer_cmd("pause");
-  print("<a class=\"pause\" href=\"?cmd=pause\">pause</a>\n");
-  goto _END;
-}
-
-# cmd empty, start a video
-if (is_file($path))
+if ($cmd == 'loadfile' && is_file($path))
 {
   # turn screen on
   send_xset_cmd("wakeup");
   # play movie
   send_mplayer_cmd("loadfile \"$path\"");
-  print("<a class=\"pause\" href=\"?path=".urlencode($path)."&cmd=pause\">pause</a>\n");
-  goto _END;
+  # change path to parent directory.
+  $path = dirname($path);
 }
-
-_LISTFILES:
 
 # default directory
 if (!is_dir($path))
@@ -96,19 +107,14 @@ if ($h = opendir($path))
   #print("<h6>$path</h6>\n");
   print("<ul data-role=\"listview\" data-filter=\"true\">\n");
   while (false !== ($entry = readdir($h))) {
-    print("<li><a href=\"?path=".urlencode(realpath("$path/$entry"))."\">$entry</a></li>\n");
+    print("<li><a href=\"?cmd=loadfile&path=".urlencode(realpath("$path/$entry"))."\">$entry</a></li>\n");
   }
   closedir($handle);
   print("</ul>\n");
-  goto _END;
 }
 
-_ERROR:
+$_SESSION['path'] = $path;
 
-print("there was an error or something...");
-exit(1);
-
-_END:
 ?>
 </div>
 </div>
